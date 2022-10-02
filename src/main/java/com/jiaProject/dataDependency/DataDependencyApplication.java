9ae238d00ce92de85f2097ed4ceb5b9c47d4b7e5
jiaLEE <jialee@marketboro.com>
2022-10-02 14:56:59 +0900

    Initial commit

---
+++ src/main/java/com/jiaProject/dataDependency/DataDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.jiaProject.dataDependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDependencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiaProject/dataDependency/DataDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaProject.dataDependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
